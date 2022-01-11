e03a3ef24bcfd94dd595614128401508e24f4a5b
Henrique Maia <henriquemaia8725@hotmail.com>
2022-01-11 20:56:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Paginition from "components/Pagination";
 
-function Listing(){
-    
-    return(
-        <Paginition />
+function Listing() {
+
+    return (
+        <>
+            <Paginition />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
 
